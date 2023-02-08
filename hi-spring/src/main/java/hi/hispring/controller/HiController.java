7206de80d83d1ef2b38541d5ff76aff4aae5dba7
wantSome <kihyun1998@naver.com>
2023-02-08 20:23:55 +0900

    Update HiController.java

--- hi-spring/src/main/java/hi/hispring/controller/HiController.java
+++ hi-spring/src/main/java/hi/hispring/controller/HiController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HiController {
@@ -18,4 +19,31 @@ public class HiController {
         model.addAttribute("name",name);
         return "hi-template";
     }
+
+    @GetMapping("hi-spring") //API 방식
+    @ResponseBody
+    public String hiString(@RequestParam("name") String name){
+        return "hi " + name;
+    }
+
+    @GetMapping("hi-api")
+    @ResponseBody
+    public Hi hiApi(@RequestParam("name") String name){
+        Hi hi = new Hi();
+        hi.setName(name);
+        return hi;
+    }
+    static class Hi {
+
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+
 }
